14e073b53f2ea73405928e11200e4d5965fe2399
Locke Monstah <Mloughran87@gmail.com>
2018-10-16 18:23:32 -0400

    timer and startbutton, show hide

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,5 @@
 
-// var questions = [
-//     {
-//         prompt: "What color are apples?\n(a) Red/green\n\
-//         (b) Purple\n(c) Orange",
-//         answer: "a"
-//     },
-//     {
-//         prompt: "What color are Bananas?\n(a) Teal\n\
-//          (b) Magenta\n (c) Yellow",
-//         answer: "c"
-//     },
-//     {
-//         prompt: "What island was Napoleon banished to?\n(a) Elba\n\
-//         (b) Santorino\n (c) Bermuda",
-//     }
-// ];
+// var questions = 
 
 // var score = 0;
 
@@ -22,33 +7,32 @@
 //     var response = window.prompt(questions[i].prompt);
 //     if (response == questions[i].answer){
 //         score++;
-//         alert("Correct");
+//        
     
-//     }
-
-//     else {
-//         alert("Wrong!");
-//     }
+//  
 
 // }
 var number = 100;
+var quizbody;
 
 function run() {
     intervalId = setInterval(decrement, 1000);
+    $(".quizbody").show();
   }
 
 function decrement() {
 
-    number--;
+     number--;
 
-    $("countdown").html("<h2>" + number + "</h2>");
+     $("#countdown").html ( "Time Remaining:" + number );
 
-    if (number === 0) {
+    // if (number === 0) {
 
-      stop();
+    //   stop();
 
-      alert("Time Up!");
-    }
+    //   alert("Time Up!");
+  console.log(number);
+    // }
 }
 
 function stop() {
@@ -56,4 +40,10 @@ function stop() {
     clearInterval(intervalId);
   }
 
-  run();
+
+//   run();
+
+//   decrement();
+
+
+$("#startbutton").click (run);
